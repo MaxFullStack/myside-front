d31ea1d560b320a3f6db97d09d45942047aa815a
Max Vinicius <app.maxvinicius@gmail.com>
2024-09-19 12:51:58 -0300

    feat: add styled-components

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import StyledComponentsRegistry from '@/lib/registry';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '@/styles/globals';
+import { theme } from '@/styles/theme';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <StyledComponentsRegistry>
+          <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            {children}
+          </ThemeProvider>
+        </StyledComponentsRegistry>
+      </body>
+    </html>
+  );
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,18 @@
+export const theme = {
+  colors: {
+    primary: '#ff3b49',  
+    secondary: '#002d40',
+    background: '#f5f5f5',
+    text: '#333',
+  },
+  fontSizes: {
+    small: '0.875rem',
+    medium: '1rem',
+    large: '1.25rem',
+  },
+  spacing: {
+    small: '8px',
+    medium: '16px',
+    large: '24px',
+  },
+};
